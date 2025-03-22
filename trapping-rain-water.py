1be9bdb5aca088d942a6636b32454dd86b2a0ffb
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-03-22 17:32:06 +0100

    Update trapping-rain-water.py

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -1,13 +1,21 @@
 class Solution:
     def trap(self, heights: List[int]) -> int:
-        return sum(
-            (min(left_height, right_height) - height)
-            for left_height, right_height, height in (
-                (
-                    max(heights[:index + 1]),
-                    max(heights[index:]),
-                    height
-                )
-                for index, height in enumerate(heights)
-            )
-        )
+        if not heights:
+            return 0
+        
+        water = 0
+        left, right = 0, len(heights) - 1
+        max_left, max_right = heights[left], heights[right]
+        
+        while left <= right:
+            max_left = max(max_left, heights[left])
+            max_right = max(max_right, heights[right])
+            
+            if max_left < max_right:
+                water += max_left - heights[left]
+                left += 1
+            else:
+                water += max_right - heights[right]
+                right -= 1
+        
+        return water
