831deabd1fa32297aa92e1f671c02265a643e0f8
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-01 21:09:59 +0200

    Update 4sum.py

--- 4sum.py
+++ 4sum.py
@@ -1,7 +1,18 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        return list({
-            tuple(sorted(combination))
-            for combination in combinations(nums, 4)
-            if sum(combination) == target
-        })
+        nums_count = len(nums)
+        result = set()
+
+        for index1 in range(0, nums_count - 3):
+            target1 = target - nums[index1]
+            for index2 in range(index1 + 1, nums_count - 2):
+                target2 = target1 - nums[index2]
+                for index3 in range(index2 + 1, nums_count - 1):
+                    target3 = target2 - nums[index3]
+                    for index4 in range(index3 + 1, nums_count):
+                        target4 = target3 - nums[index4]
+                        if target4 == 0:
+                            result.add(tuple(sorted((nums[index1], nums[index2], nums[index3], nums[index4]))))
+
+        return list(result)
+
