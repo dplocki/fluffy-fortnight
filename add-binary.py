3a45c0a8860b23fc4e0e9f0de5ac7d29a0bbfb8d
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-03 19:07:52 +0200

    Create add-binary.py

---
+++ add-binary.py
@@ -0,0 +1,28 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_index, b_index = len(a) -1, len(b) - 1 
+        result = ''
+        results = {
+            ('0', '0', '0'): ('0', '0'),
+            ('0', '0', '1'): ('1', '0'),
+            ('1', '0', '0'): ('1', '0'),
+            ('1', '0', '1'): ('0', '1'),
+            ('0', '1', '0'): ('1', '0'),
+            ('0', '1', '1'): ('0', '1'),
+            ('1', '1', '0'): ('0', '1'),
+            ('1', '1', '1'): ('1', '1'),
+        }
+
+        carring = '0'
+
+        while a_index >= 0 or b_index >= 0 or carring == '1':
+            first = a[a_index] if a_index >= 0 else '0'
+            second = b[b_index] if b_index >= 0 else '0'
+
+            digit, carring = results[first, second, carring]
+            result = digit + result
+
+            a_index -= 1
+            b_index -= 1
+
+        return result
