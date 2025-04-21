44e3576628757b6da17cfbbfaf5ad0ba07786c2b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-21 13:03:53 +0200

    Update jump-game-ii.py

--- jump-game-ii.py
+++ jump-game-ii.py
@@ -1,13 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         last_index = len(nums) - 1
-        dp = { 0: 0 }
-        for index, num in enumerate(nums):
-            will_be_jumps = dp.get(index, 0) + 1
+        near = index = jumps = 0
 
-            for delta in range(1, num + 1):
-                new_index = index + delta
-                if new_index <= last_index and (new_index not in dp or will_be_jumps < dp[new_index]):
-                    dp[new_index] = will_be_jumps
+        while index < last_index:
+            farthest = max(i + nums[i] for i in range(near, index + 1))
+            near = index + 1
+            index = farthest
+            jumps += 1
 
-        return dp[last_index]
+        return jumps
