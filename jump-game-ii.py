dc9ae044f78dd5311d562378f5512b291457eb9a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-21 11:09:23 +0200

    Update jump-game-ii.py

--- jump-game-ii.py
+++ jump-game-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         last_index = len(nums) - 1
-        possibilites = deque([(0, 0)])
+        dp = { 0: 0 }
+        for index, num in enumerate(nums):
+            will_be_jumps = dp.get(index, 0) + 1
 
-        while possibilites:
-            current_index, current_jumps_count = possibilites.popleft()
-            if current_index == last_index:
-                return current_jumps_count
+            for delta in range(1, num + 1):
+                new_index = index + delta
+                if new_index <= last_index and (new_index not in dp or will_be_jumps < dp[new_index]):
+                    dp[new_index] = will_be_jumps
 
-            next_jump_count = current_jumps_count + 1
-            for delta in range(1, nums[current_index] + 1):
-                possibilites.append((current_index + delta, next_jump_count))
+        return dp[last_index]
