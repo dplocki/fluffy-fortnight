05c148073b394b84d5060aa64b0b0d9938767b43
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-26 08:39:21 +0200

    Update decode-xored-array.py

--- decode-xored-array.py
+++ decode-xored-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
         result = [first]
-        for i in range(len(encoded)):
-            result.append(encoded[i] ^ result[i])
+        for item in encoded:
+            result.append(result[-1] ^ item)
 
         return result
