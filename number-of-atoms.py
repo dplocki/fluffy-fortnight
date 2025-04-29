b23d2f3cd4d2c93e7c0c1efb96fbdb7cb4f668f7
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-04-29 17:40:40 +0200

    Update number-of-atoms.py

--- number-of-atoms.py
+++ number-of-atoms.py
@@ -1,29 +1,31 @@
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        tokens = re.findall(r'[A-Z][a-z]?|\d+|[()]', formula)
+        tokens = re.findall(r'[A-Z][a-z]*|\d+|[()]', formula)
+        tokens.append('$')
         stack = [Counter()]
-
+        
         multiplayer = 1
         for index, token in enumerate(tokens):
-            if token == '(':
-                stack.append(Counter())
-            elif token == ')':
-                pass
-            
             if token.isdigit():
                 multiplayer = int(token)
             else:
                 multiplayer = 1
 
-            if tokens[index - 1] == ')':
-                bracket_result = stack.pop()
-                for atom in bracket_result:
-                    stack[-1][atom] = stack[-1][atom] + bracket_result.get(atom, 0) * multiplayer
-            else:
-                last_symbol = tokens[index - 1]
-                stack[-1][last_symbol] = stack[-1][last_symbol] + (multiplayer - 1)
+            if not tokens[index - 1].isdigit():
+                if tokens[index - 1] == ')':
+                    bracket_result = stack.pop()
+                    for atom in bracket_result:
+                        stack[-1][atom] = stack[-1][atom] + bracket_result.get(atom, 0) * multiplayer
+                elif tokens[index - 1] == '(' or tokens[index - 1] == '$':
+                    pass
+                else:
+                    last_symbol = tokens[index - 1]
+                    stack[-1][last_symbol] = stack[-1][last_symbol] + multiplayer
 
-            if not token.isdigit():
-                stack[-1][token] = stack[-1].get(token, 0) + 1
+            if token == '$':
+                break
+
+            if token == '(':
+                stack.append(Counter())
 
         return ''.join(key + (str(stack[0][key]) if stack[0][key] > 1 else '') for key in sorted(stack[0]))
