0ddc98215f8696ff8ac3c9c3bda58c63581f6313
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-05-05 19:22:59 +0200

    Update domino-and-tromino-tiling.py

--- domino-and-tromino-tiling.py
+++ domino-and-tromino-tiling.py
@@ -2,13 +2,20 @@ MOD = 10**9 + 7
 
 class Solution:
     def numTilings(self, n: int) -> int:
+        self.cache = {}
         return self.find_ways_count(n, 0, False)
 
     def find_ways_count(self, n: int, index: int, gap: bool) -> int:
+        if (index, gap) in self.cache:
+            return self.cache[index, gap]
+
         if index > n:
+            self.cache[index, gap] = 0
             return 0
 
         if index == n:
+            self.cache[index, True] = 0
+            self.cache[index, False] = 1
             return 0 if gap else 1
 
         result = 0
@@ -20,4 +27,6 @@ class Solution:
             result += self.find_ways_count(n, index + 2, False)
             result += 2 * self.find_ways_count(n, index + 2, True)
 
-        return result % MOD
+        result %= MOD
+        self.cache[index, gap] = result
+        return result
