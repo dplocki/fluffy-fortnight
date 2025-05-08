497839d0b3e5197e5c0488aae8b85987bebc309d
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-05-08 20:14:56 +0200

    Update generate-parentheses.py

--- generate-parentheses.py
+++ generate-parentheses.py
@@ -1,13 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        return list(self.internal(n, 0, 0, ''))
+        return list(self.internal(n, n, ''))
 
-    def internal(self, n: int,  open_bracket_count: int, close_bracket_count: int, combination: str):
-        if open_bracket_count > n or close_bracket_count > n or close_bracket_count > open_bracket_count:
+    def internal(self, open_bracket_count: int, close_bracket_count: int, combination: str):
+        if open_bracket_count < 0 or close_bracket_count < 0 or close_bracket_count < open_bracket_count:
             return
 
-        if open_bracket_count == n == close_bracket_count:
+        if open_bracket_count == close_bracket_count == 0:
             yield combination
         else:
-            yield from self.internal(n, open_bracket_count + 1, close_bracket_count, combination + '(')
-            yield from self.internal(n, open_bracket_count, close_bracket_count + 1, combination + ')')
+            yield from self.internal(open_bracket_count - 1, close_bracket_count, combination + '(')
+            yield from self.internal(open_bracket_count, close_bracket_count - 1, combination + ')')
