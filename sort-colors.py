c82c5f1467555e7daa0b68e54bbf8013c8fef5f0
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-05-17 08:08:42 +0200

    Update sort-colors.py

--- sort-colors.py
+++ sort-colors.py
@@ -3,4 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        left, middle, right = 0, 0, len(nums) - 1
+
+        while middle <= right:
+            if nums[middle] == 0:
+                nums[left], nums[middle] = nums[middle], nums[left]
+                left += 1
+                middle += 1
+            elif nums[middle] == 2:
+                nums[right], nums[middle] = nums[middle], nums[right]
+                right -= 1
+            else:
+                middle += 1
+
+        return nums
