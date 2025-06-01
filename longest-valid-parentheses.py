bd9c2aed6ce2908886ea564c5815d71b38cd1051
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-01 14:19:52 +0200

    Update longest-valid-parentheses.py

--- longest-valid-parentheses.py
+++ longest-valid-parentheses.py
@@ -3,13 +3,18 @@ class Solution:
         dp = defaultdict(int)
         dp[0] = 0
 
-        for index, character in enumerate(s):
-            if character == ')':
-                if index > 0 and s[index - 1] == '(':
-                    dp[index] = dp[index - 2] + 2
-                else:
-                    potentail_close_index = index - dp[index - 1] - 1
-                    if potentail_close_index >= 0 and s[potentail_close_index] == '(':
-                        dp[index] = dp[index - 1] + 2 + dp[potentail_close_index - 1]
+        iterator = iter(enumerate(s))
+        next(iterator, None)
+
+        for index, character in iterator:
+            if character == '(':
+                continue
+
+            if s[index - 1] == '(':
+                dp[index] = dp[index - 2] + 2
+            else:
+                potentail_close_index = index - dp[index - 1] - 1
+                if potentail_close_index >= 0 and s[potentail_close_index] == '(':
+                    dp[index] = dp[index - 1] + 2 + dp[potentail_close_index - 1]
 
         return max(dp.values())
