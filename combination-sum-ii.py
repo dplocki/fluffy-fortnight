c039135d9d8da92d83f04e847c4335f117d9db49
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-02 20:00:18 +0200

    Update combination-sum-ii.py

--- combination-sum-ii.py
+++ combination-sum-ii.py
@@ -1,18 +1,22 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         max_candidates = len(candidates)
+        current = []
 
-        def internal(target, start_index, current):
+        def internal(target, start_index):
             if target == 0:
                 yield tuple(current)
                 return
 
             for index in range(start_index, max_candidates):
                 candidate = candidates[index]
+    
+                if index > start_index and candidate == candidates[index - 1]:
+                    continue
 
                 current.append(candidate)
-                yield from internal(target - candidate, index + 1, current)
+                yield from internal(target - candidate, index + 1)
                 current.pop()
 
         candidates.sort()
-        return list(set(internal(target, 0, [])))
+        return list(internal(target, 0))
