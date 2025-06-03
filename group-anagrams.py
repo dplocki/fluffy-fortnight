a100e727831b8f4281c7979d4f77055a3d4da71c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-03 18:13:53 +0200

    Update group-anagrams.py

--- group-anagrams.py
+++ group-anagrams.py
@@ -3,6 +3,6 @@ class Solution:
         result = defaultdict(list)
 
         for single_str in strs:
-            result[''.join(sorted(single_str))].append(single_str)
+            result[tuple(sorted(single_str))].append(single_str)
 
         return list(result.values())
