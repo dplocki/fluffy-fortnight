73910075d402a3c49670af8ca224c1aca14b6017
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-04 17:04:46 +0200

    Update 4sum.py

--- 4sum.py
+++ 4sum.py
@@ -1,18 +1,18 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums_count = len(nums)
-        result = set()
+        current = []
+        max_nums = len(nums)
 
-        for index1 in range(0, nums_count - 3):
-            target1 = target - nums[index1]
-            for index2 in range(index1 + 1, nums_count - 2):
-                target2 = target1 - nums[index2]
-                for index3 in range(index2 + 1, nums_count - 1):
-                    target3 = target2 - nums[index3]
-                    for index4 in range(index3 + 1, nums_count):
-                        target4 = target3 - nums[index4]
-                        if target4 == 0:
-                            result.add(tuple(sorted((nums[index1], nums[index2], nums[index3], nums[index4]))))
+        def internal(start_index):
+            if len(current) == 4 and sum(current) == target:
+                yield tuple(current)
+                return
 
-        return list(result)
+            for index in range(start_index, max_nums):
+                if index > 0 or nums[index] != nums[index - 1]:
+                    current.append(nums[index])
+                    yield from internal(index + 1)
+                    current.pop()
 
+        nums.sort()
+        return list(internal(0))
