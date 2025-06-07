c8b353fbf2e20bf0a4fbe05c9ed2a1b937531bb9
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-07 07:32:07 +0200

    Update gray-code.py

--- gray-code.py
+++ gray-code.py
@@ -1,15 +1,6 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        current = [0]
-
-        for multiplayer in range(n):
-            addition = 2 ** multiplayer
-
-            new_current = current[:]
-            
-            for c in reversed(current):
-                new_current.append(addition + c)
-            
-            current = new_current
-        
-        return current
+        return [
+            i ^ (i >> 1)
+            for i in range(1 << n)
+        ]
