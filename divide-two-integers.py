ef8af4b8c5cb17e3444d4005e04221e134b37c32
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-09 17:20:27 +0200

    Update divide-two-integers.py

--- divide-two-integers.py
+++ divide-two-integers.py
@@ -1,38 +1,30 @@
 class Solution:
+    MIN = -2**31
+    MAX = 2**31 - 1
+
     def divide(self, dividend: int, divisor: int) -> int:
-        result = self.internal(dividend, divisor)
-        if result > (2**31 - 1):
-            return 2**31 - 1
-        if result < -2**31:
-            return -2**31
-
-        return result
-
-    def internal(self, dividend: int, divisor: int):
         result = 0
-        current = 0
-        is_negative = False
+        dividend_sign = 1 if dividend > 0 else -1
+        divisor_sign = 1 if divisor > 0 else -1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
 
-        if divisor < 0:
-            divisor = -divisor
-            is_negative = True
+        if divisor > 1:
+            while True:
+                dividend -= divisor
+                if dividend >= 0:
+                    result += 1
+                else:
+                    break
+        else:
+            result = dividend
 
-        if dividend < 0:
-            dividend = -dividend
-            is_negative = not is_negative
+        if dividend_sign != divisor_sign:
+            result = -result
 
-        if divisor == 1:
-            if is_negative:
-                return -dividend
-            else:
-                return dividend
-
-        while True:
-            current += divisor
-            if current > dividend:
-                return result
-
-            if is_negative:
-                result -= 1
-            else:
-                result += 1
+        if result > Solution.MAX:
+            result = Solution.MAX
+        elif result < Solution.MIN:
+            result = Solution.MIN
+        
+        return result
