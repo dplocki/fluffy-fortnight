78412a91ceed342aa2b0667f286a7b1033162b4a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-12 18:01:55 +0200

    Create simplify-path.py

---
+++ simplify-path.py
@@ -0,0 +1,16 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        result = []
+        for token in path.split('/'):
+            if not token:
+                continue
+            elif token == '.':
+                continue
+            elif token == '..':
+                if result:
+                    result.pop()
+                continue
+            
+            result.append(token)
+
+        return '/' + '/'.join(result)
