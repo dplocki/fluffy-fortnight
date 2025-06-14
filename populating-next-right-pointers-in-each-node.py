da48ccccf48bbde79f56007cc43640c3f7b4f05c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-14 09:49:49 +0200

    Create populating-next-right-pointers-in-each-node.py

---
+++ populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+
+        level = [root, None]
+        while len(level) > 1:
+            new_level = []
+
+            for node, node_next in zip(level, level[1:]):
+                node.next = node_next
+
+                if node.left:
+                    new_level.append(node.left)
+
+                if node.right:
+                    new_level.append(node.right)
+
+            new_level.append(None)
+            level = new_level
+
+        return root
