20b8ab58dd87f459acd9e22e6239d9a1ef8d994b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-17 19:25:46 +0200

    Update edit-distance.py

--- edit-distance.py
+++ edit-distance.py
@@ -11,13 +11,12 @@ class Solution:
             dp[index_1, 0] = index_1
 
             for index_2 in range(1, word2_length + 1):
-                if word1[index_1 - 1] == word2[index_2 - 1]:
-                    dp[index_1, index_2] = dp[index_1 - 1, index_2 - 1]
-                else:
-                    dp[index_1, index_2] = min(
-                        dp[index_1 - 1, index_2],
-                        dp[index_1, index_2 - 1],
-                        dp[index_1 - 1, index_2 - 1]
-                    ) + 1
+                dp[index_1, index_2] = dp[index_1 - 1, index_2 - 1] \
+                    if word1[index_1 - 1] == word2[index_2 - 1] \
+                    else min(
+                            dp[index_1 - 1, index_2],
+                            dp[index_1, index_2 - 1],
+                            dp[index_1 - 1, index_2 - 1]
+                        ) + 1
         
         return dp[word1_length, word2_length]
