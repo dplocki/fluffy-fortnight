80d7f0b4ccbefe0c43baa06bc23ffc45572113e1
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-20 13:28:46 +0200

    Update valid-number.py

--- valid-number.py
+++ valid-number.py
@@ -1,3 +1,5 @@
 class Solution:
+    IS_NUMBER = re.compile(r'^(-|\+)?((\d+\.\d*)|(\d+)|(\d*\.\d+))((e|E)(\+|-|)\d+)?$')
+
     def isNumber(self, s: str) -> bool:
-        return bool(re.match(r'^(-|\+)?((\d+\.\d*)|(\d+)|(\d*\.\d+))((e|E)(\+|-|)\d+)?$', s))
+        return bool(re.match(Solution.IS_NUMBER, s))
