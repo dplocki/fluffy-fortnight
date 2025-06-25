1977dd62ab2a9c7ce1f405ed8809e0439ccec374
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-25 19:10:52 +0200

    Create n-queens.py

---
+++ n-queens.py
@@ -0,0 +1,25 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        all_elements = set(range(n))
+
+        def can_queen_be_place(row, columns, current_column):
+            for prev_row in range(row):
+                if columns[row - prev_row - 1] in (current_column - prev_row - 1, prev_row + current_column + 1, current_column):
+                    return False
+
+            return True
+
+        def internal(columns):
+            row = len(columns)
+            if row == n:
+                yield columns
+                return
+
+            for column in range(n):
+                if can_queen_be_place(row, columns, column):
+                    yield from internal(columns + [column])
+
+        return [
+            [('.' * s) + 'Q' + ('.' * (n - s - 1)) for s in solution ]
+            for solution in internal([])
+        ]
