19a4af72f03a1bc93da014ec5f8269c9dda0d0a0
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-25 19:27:36 +0200

    Update n-queens.py

--- n-queens.py
+++ n-queens.py
@@ -1,7 +1,5 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        all_elements = set(range(n))
-
         def can_queen_be_place(row, columns, current_column):
             for prev_row in range(row):
                 if columns[row - prev_row - 1] in (current_column - prev_row - 1, prev_row + current_column + 1, current_column):
@@ -20,6 +18,6 @@ class Solution:
                     yield from internal(columns + [column])
 
         return [
-            [('.' * s) + 'Q' + ('.' * (n - s - 1)) for s in solution ]
+            [('.' * s) + 'Q' + ('.' * (n - s - 1)) for s in solution]
             for solution in internal([])
         ]
