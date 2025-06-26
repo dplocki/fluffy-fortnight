215550ed4883b68d01ee9e8cee9679a4a67da34c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-26 21:23:53 +0200

    Create n-queens-ii.py

---
+++ n-queens-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def can_queen_be_place(row, columns, current_column):
+            for prev_row in range(row):
+                if columns[row - prev_row - 1] in (current_column - prev_row - 1, prev_row + current_column + 1, current_column):
+                    return False
+
+            return True
+
+        def internal(columns):
+            row = len(columns)
+            if row == n:
+                yield columns
+                return
+
+            for column in range(n):
+                if can_queen_be_place(row, columns, column):
+                    yield from internal(columns + [column])
+
+        return sum(1 for solution in internal([]))
