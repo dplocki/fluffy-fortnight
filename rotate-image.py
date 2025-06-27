c89d13cf724d4bcf824560da98f167537e89d926
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-27 16:00:23 +0200

    Create rotate-image.py

---
+++ rotate-image.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        size = len(matrix)
+        
+        for row in range(size >> 1):
+            for column in range(size):
+                matrix[row][column], matrix[size - row - 1][column] = matrix[size - row - 1][column], matrix[row][column]
+
+        for row in range(size):
+            for column in range(row):
+                matrix[row][column], matrix[ column][row] = matrix[column][row], matrix[row][column]
