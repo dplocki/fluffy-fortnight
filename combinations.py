b05a9da12cd7f35399a5fae3b2699a8d3f8f9be7
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-06-29 11:57:02 +0200

    Update combinations.py

--- combinations.py
+++ combinations.py
@@ -1,4 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        numbers = [i + 1 for i in range(n)]
-        return list(combinations(numbers, k))
+        current = []
+
+        def internal(number: int):
+            current_size = len(current)
+            if current_size == k:
+                yield tuple(current)
+                return
+            elif current_size > k:
+                return
+
+            if number > n:
+                return
+
+            current.append(number)
+            yield from internal(number + 1)
+            current.pop()
+            yield from internal(number + 1)
+
+        return list(internal(1))
