536f977eb6264c053a25c9a3d8496d4a7fa334be
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-07-18 20:10:10 +0200

    Update majority-element.py

--- majority-element.py
+++ majority-element.py
@@ -1,3 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums) >> 1]
+        candidate = None
+        counter = 0
+
+        for num in nums:
+            if counter == 0:
+                candidate = num
+                counter = 1
+            elif num == candidate:
+                counter += 1
+            else:
+                counter -= 1
+
+        return candidate
