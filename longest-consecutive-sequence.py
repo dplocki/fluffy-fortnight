d9e77ecbac7c1eac067106785f6b1935d7181bbf
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-07-29 16:51:10 +0200

    Update longest-consecutive-sequence.py

--- longest-consecutive-sequence.py
+++ longest-consecutive-sequence.py
@@ -1,7 +1,10 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
         numbers = set(nums)
-        result = 0
+        result = 1
 
         for number in numbers:
             if number - 1 not in numbers:
@@ -10,7 +13,7 @@ class Solution:
             current_number = number + 1
             while current_number in numbers:
                 current_number += 1
-
+            
             result = max(result, current_number - number + 1)
 
         return result
