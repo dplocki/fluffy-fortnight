446220d9493f4af354ae15dfadcd78a77aefef1a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-08-03 12:03:37 +0200

    Update word-ladder-ii.py

--- word-ladder-ii.py
+++ word-ladder-ii.py
@@ -2,11 +2,7 @@ class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         connections_map = self.build_connection_map(wordList + [beginWord])
         paths = list(self.find_paths(connections_map, beginWord, endWord))
-        if not paths:
-            return paths
-
-        min_length = min(len(p) for p in paths)
-        return [p for p in paths if len(p) == min_length]
+        return paths
 
     def build_connection_map(self, wordList):
         connections = defaultdict(set)
@@ -19,6 +15,7 @@ class Solution:
     def find_paths(self, connections: Dict[str, Set], beginWord: str, endWord: str):
         to_check = deque([(beginWord,)])
         path_min = 500
+        visited = set()
 
         while to_check:
             path = to_check.popleft()
@@ -36,8 +33,10 @@ class Solution:
                 continue
 
             for neighbor in connections[last_path]:
-                if neighbor not in path:
+                if neighbor not in visited:
                     to_check.append((*path, neighbor))
+
+            visited.add(last_path)
 
     def differ_by_one_char(self, word1: str, word2: str):
         return sum(c1 != c2 for c1, c2 in zip(word1, word2)) == 1
