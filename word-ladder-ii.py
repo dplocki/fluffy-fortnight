5c05639e4e8920c9ff969aabc469e0ae2058ae58
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-08-04 19:10:30 +0200

    Update word-ladder-ii.py

--- word-ladder-ii.py
+++ word-ladder-ii.py
@@ -1,42 +1,53 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
         connections_map = self.build_connection_map(wordList + [beginWord])
-        paths = list(self.find_paths(connections_map, beginWord, endWord))
-        return paths
+        parents_map = self.find_paths(connections_map, beginWord, endWord)
+        return self.build_paths(parents_map, beginWord, endWord)
 
-    def build_connection_map(self, wordList):
+    def build_connection_map(self, word_list):
         connections = defaultdict(set)
-        for word_a, word_b in combinations(wordList, 2):
+        for word_a, word_b in combinations(word_list, 2):
             if self.differ_by_one_char(word_a, word_b):
                 connections[word_a].add(word_b)
                 connections[word_b].add(word_a)
         return connections
 
-    def find_paths(self, connections: Dict[str, Set], beginWord: str, endWord: str):
-        to_check = deque([(beginWord,)])
-        path_min = 500
-        visited = set()
+    def build_paths(self, parents_map, begin_word: str, end_word: str):
+        def backtrack(word):
+            if word == begin_word:
+                return [(begin_word,)]
+            
+            paths = []
+            for parent in parents_map[word]:
+                for path in backtrack(parent):
+                    paths.append((*path, word))
+            
+            return paths
+        
+        return backtrack(end_word)
 
-        while to_check:
-            path = to_check.popleft()
-            path_size = len(path)
-            last_path = path[-1]
+    def find_paths(self, connections: Dict[str, Set], begin_word: str, end_word: str):
+        current_level = {begin_word}
+        visited = {begin_word}
+        parents = defaultdict(set)
 
-            if path_size > path_min:
-                continue
+        while current_level and end_word not in current_level:
+            next_level = set()
+            level_visited = set()
 
-            if last_path == endWord:
-                if path_min > path_size:
-                    path_min = path_size
+            for current_word in current_level:
+                for next_word in connections[current_word]:
+                    if next_word not in visited and next_word not in level_visited:
+                        next_level.add(next_word)
+                        level_visited.add(next_word)
+                    
+                    if next_word not in visited:
+                        parents[next_word].add(current_word)
+            
+            visited.update(level_visited)
+            current_level = next_level
 
-                yield path
-                continue
-
-            for neighbor in connections[last_path]:
-                if neighbor not in visited:
-                    to_check.append((*path, neighbor))
-
-            visited.add(last_path)
+        return parents
 
     def differ_by_one_char(self, word1: str, word2: str):
         return sum(c1 != c2 for c1, c2 in zip(word1, word2)) == 1
