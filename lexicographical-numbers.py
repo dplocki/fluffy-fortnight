cf74c31c44a44b9c3e6cd223fc686752111ef140
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-08-13 18:57:38 +0200

    Update lexicographical-numbers.py

--- lexicographical-numbers.py
+++ lexicographical-numbers.py
@@ -1,3 +1,16 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        return list(map(int, sorted(map(str, list(range(1, n + 1))))))
+        return list(self.generate_lexical_order_numbers(n))
+
+    def generate_lexical_order_numbers(self, n):
+        current_value = 1
+        for _ in range(n):
+            yield current_value
+
+            if current_value * 10 <= n:
+                current_value *= 10
+            else:
+                while current_value % 10 == 9 or current_value + 1 > n:
+                    current_value //= 10
+
+                current_value += 1
