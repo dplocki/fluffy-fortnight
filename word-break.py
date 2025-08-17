f78e1438323dd348a07d83c2789e0202d93b7b76
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-08-17 14:31:56 +0200

    Update word-break.py

--- word-break.py
+++ word-break.py
@@ -1,16 +1,15 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
+        dp = { 0: True }
 
-        @cache
-        def internal(subword: str) -> bool:
-            if subword in words:
-                return True
+        for end in range(1, len(s) + 1):
+            for start in range(end):
+                if not dp.get(start, False):
+                    continue
 
-            for split_index in range(1, len(subword)):
-                if internal(subword[:split_index]) and internal(subword[split_index:]):
-                    return True
+                if s[start:end] in words:
+                    dp[end] = True
+                    break
 
-            return False
-
-        return internal(s)
+        return dp.get(len(s), False)
