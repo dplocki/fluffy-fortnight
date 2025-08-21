f1c19786a5b28973bc7858f06baecb1c38b4aec6
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-08-21 17:40:57 +0200

    Update count-submatrices-with-all-ones.py

--- count-submatrices-with-all-ones.py
+++ count-submatrices-with-all-ones.py
@@ -1,19 +1,12 @@
 class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
-        result = 0
-
         vertical_splites = {}
         for row_index, row in enumerate(mat):
             for column_index, value in enumerate(row):
                 vertical_splites[row_index, column_index] = 0 if value == 0 else 1 + vertical_splites.get((row_index, column_index - 1), 0)
 
-        for row_index, row in enumerate(mat):
-            for column_index, value in enumerate(row):
-                local_result = vertical_splites[row_index, column_index]
-                result += local_result
-
-                for k in range(row_index - 1, -1, -1):
-                    local_result = min(local_result, vertical_splites.get((k, column_index), 0))
-                    result += local_result
-
-        return result
+        return sum(
+            sum(accumulate((vertical_splites.get((k, column_index), 0) for k in range(row_index, -1, -1)), min))
+            for row_index, row in enumerate(mat)
+            for column_index, value in enumerate(row)
+        )
