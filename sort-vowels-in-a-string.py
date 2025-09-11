7d8337a32a2634b3cde5e95c608fe43bd5e283de
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-11 07:13:48 +0200

    Update sort-vowels-in-a-string.py

--- sort-vowels-in-a-string.py
+++ sort-vowels-in-a-string.py
@@ -2,13 +2,10 @@ class Solution:
     VOWELS = set('aAeEiIoOuU')
 
     def sortVowels(self, s: str) -> str:
-        vowels = []
+        vowels = iter(sorted(letter
+            for letter in s
+            if letter in Solution.VOWELS))
 
-        for letter in s:
-            if letter in Solution.VOWELS:
-                vowels.append(letter)
-
-        vowels = iter(sorted(vowels))
         return ''.join(
             next(vowels) if letter in Solution.VOWELS else letter
             for letter in s)
