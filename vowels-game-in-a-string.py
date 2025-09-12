9c8e49be75865dbf274ea56eca897a04aed3eafe
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-12 09:33:13 +0200

    Update vowels-game-in-a-string.py

--- vowels-game-in-a-string.py
+++ vowels-game-in-a-string.py
@@ -2,4 +2,4 @@ class Solution:
     VOWELS = set('aeiou')
 
     def doesAliceWin(self, s: str) -> bool:
-        return sum(1 for l in s if l in Solution.VOWELS) > 0
+        return bool(set(s) & Solution.VOWELS)
