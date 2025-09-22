cb21b1ed90638d04baa75b0189d925297bde3ad7
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-22 17:48:14 +0200

    Update count-elements-with-maximum-frequency.py

--- count-elements-with-maximum-frequency.py
+++ count-elements-with-maximum-frequency.py
@@ -1,9 +1,10 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         counter = Counter(nums)
-        maximum = max(counter.values())
+        counter_values = counter.values()
+        maximum = max(counter_values)
 
         return sum(
             how_many
-            for element, how_many in counter.items()
+            for how_many in counter_values
             if how_many == maximum)
