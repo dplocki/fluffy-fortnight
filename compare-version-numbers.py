d0669d5b2e7803704f4b4ddc67b0c0ce6d681d40
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-23 07:49:15 +0200

    Create compare-version-numbers.py

---
+++ compare-version-numbers.py
@@ -0,0 +1,9 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        for v1, v2 in zip_longest(map(int, version1.split('.')), map(int, version2.split('.')), fillvalue=0):
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+
+        return 0
