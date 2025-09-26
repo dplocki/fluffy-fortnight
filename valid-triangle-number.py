558806d2bf543e801162856f17e7f710b5938148
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-26 20:11:24 +0200

    Update valid-triangle-number.py

--- valid-triangle-number.py
+++ valid-triangle-number.py
@@ -1,5 +1,13 @@
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        return sum(1
-            for a, b, c in combinations(nums, 3)
-            if a < b + c and b < a + c and c < a + b)
+        result = 0
+        nums.sort()
+        max_nums = len(nums)
+
+        for first_side in range(max_nums - 2):
+            for second_side in range(first_side + 1, max_nums - 1):
+                limit = nums[first_side] + nums[second_side]
+                third_side = bisect_left(nums, limit, lo=second_side + 1)
+                result += third_side - 1 - second_side
+
+        return result
