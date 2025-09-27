71bd714e9052efb0e90e7109a27f1454acac5245
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-09-27 07:50:20 +0200

    Create largest-triangle-area.py

---
+++ largest-triangle-area.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        return max(
+            abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))
+            for x1, y1 in points
+            for x2, y2 in points
+            for x3, y3 in points) / 2
