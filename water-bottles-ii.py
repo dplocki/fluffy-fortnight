a2be449f2d41cfb25125a6166760877750fc88fc
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-10-02 18:34:40 +0200

    Update water-bottles-ii.py

--- water-bottles-ii.py
+++ water-bottles-ii.py
@@ -2,17 +2,10 @@ class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
         empty_bottles = numBottles
         result = numBottles
-        full_bottles = 0
 
-        while full_bottles > 0 or empty_bottles >= numExchange:
-            if full_bottles > 0:
-                result += full_bottles
-                empty_bottles += full_bottles
-                full_bottles = 0
-            
-            if empty_bottles >= numExchange:
-                full_bottles += 1
-                empty_bottles -= numExchange
-                numExchange += 1
+        while empty_bottles >= numExchange:
+            result += 1
+            empty_bottles -= numExchange - 1
+            numExchange += 1
 
         return result
