a275e9aded1d9a19c9374a34a80d7a8e8bd5c83c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-10-11 14:59:42 +0200

    Create maximum-total-damage-with-spell-casting.py

---
+++ maximum-total-damage-with-spell-casting.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumTotalDamage(self, power: List[int]) -> int:
+        power.sort()
+        dp = { -3: 0 }
+        chains = {}
+        prev = -3
+
+        for index, p in enumerate(power):
+            if index > 0 and power[index -1] == p:
+                dp[p] += p
+                continue
+
+            t = prev
+            while t >= p - 2:
+                t = chains[t]
+
+            result = dp.get(t, 0)
+            s = t
+            while s >= t - 2 and s > -1:
+                result = max(result, dp.get(s, 0))
+                s = chains[s]
+                
+            dp[p] = result + p
+            chains[p] = prev
+            prev = p
+
+        return max(dp.values())
