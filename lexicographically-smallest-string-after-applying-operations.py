5ade4cbb441acb0053e404a6714f9b53ad62f975
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-10-19 09:20:54 +0200

    Update lexicographically-smallest-string-after-applying-operations.py

--- lexicographically-smallest-string-after-applying-operations.py
+++ lexicographically-smallest-string-after-applying-operations.py
@@ -6,17 +6,15 @@ class Solution:
         
         while to_check:
             current = to_check.pop()
+            if current in visited:
+                continue
+
             if current < result:
                 result = current
 
             visited.add(current)
             
-            tmp = tuple(c if i % 2 == 0 else ((c + a) % 10) for i, c in enumerate(current))
-            if tmp not in visited:
-                to_check.append(tmp)
-
-            tmp = current[-b:] + current[:-b]
-            if tmp not in visited:
-                to_check.append(tmp)
+            to_check.append(tuple(c if i % 2 == 0 else ((c + a) % 10) for i, c in enumerate(current)))
+            to_check.append(current[-b:] + current[:-b])
 
         return ''.join(map(str, result))
