4be1b7c77e7754e2c70b6728279246ffbf178e27
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-04 21:14:16 +0100

    Create find-x-sum-of-all-k-long-subarrays-i

---
+++ find-x-sum-of-all-k-long-subarrays-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        freq = Counter(nums[:k])
+        result = [self.calculate_x_sum_from_freq(freq, x)]
+
+        for i in range(k, len(nums)):
+            freq[nums[i]] += 1
+
+            left = nums[i - k]
+            freq[left] -= 1
+            if freq[left] == 0:
+                del freq[left]
+
+            result.append(self.calculate_x_sum_from_freq(freq, x))
+
+        return result
+    
+    def calculate_x_sum_from_freq(self, freq: dict, x: int) -> int:
+        sorted_items = sorted(freq.items(), key=lambda item: (-item[1], -item[0]))
+        x_sum = sum(value * count for value, count in sorted_items[:x])
+        return x_sum
