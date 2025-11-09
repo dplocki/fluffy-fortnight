523a405696946a881ec9f4e0d38af74b67bb068a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-09 09:06:34 +0100

    Update count-operations-to-obtain-zero.py

--- count-operations-to-obtain-zero.py
+++ count-operations-to-obtain-zero.py
@@ -1,11 +1,9 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         result = 0
-        while num1 != 0 and num2 != 0:
-            result += 1
-            if num1 >= num2:
-                num1 -= num2
-            else:
-                num2 -= num1
+        while num1 and num2 :
+            result += num1 // num2
+            num1 %= num2
+            num1, num2 = num2, num1
 
         return result
