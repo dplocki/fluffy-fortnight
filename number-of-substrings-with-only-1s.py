7139384ee50aedbfc887c4f088d9acec0ce7df5e
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-16 05:51:21 +0100

    Update number-of-substrings-with-only-1s.py

--- number-of-substrings-with-only-1s.py
+++ number-of-substrings-with-only-1s.py
@@ -2,4 +2,4 @@ class Solution:
     MODULO = int(1e9 + 7)
 
     def numSub(self, s: str) -> int:
-        return sum(map(lambda l: l*(l + 1)//2, map(len, s.split('0')))) % Solution.MODULO
+        return sum(map(lambda l: l*(l + 1)>>1, map(len, s.split('0')))) % Solution.MODULO
