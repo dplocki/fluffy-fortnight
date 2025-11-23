22cf8c9a303621e1994aa1c091eb28ac9f5cde64
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-23 13:10:37 +0100

    Update greatest-sum-divisible-by-three.py

--- greatest-sum-divisible-by-three.py
+++ greatest-sum-divisible-by-three.py
@@ -1,19 +1,10 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        dp = { (-1, 0): 0, (-1, 1): 0, (-1, 2): 0 }
+        dp = [0, 0, 0]
 
-        for index, num in enumerate(nums):
-            dp[index, 0] = dp[index - 1, 0]
-            dp[index, 1] = dp[index - 1, 1]
-            dp[index, 2] = dp[index - 1, 2]
+        for num in nums:
+            for d in dp[:]:
+                x = d + num
+                dp[x % 3] = max(x, dp[x % 3])
 
-            x = dp[index - 1, 0] + num
-            dp[index, x % 3] = max(x, dp.get((index, x % 3), 0))
-
-            x = dp[index - 1, 1] + num
-            dp[index, x % 3] = max(x, dp.get((index, x % 3), 0))
-
-            x = dp[index - 1, 2] + num
-            dp[index, x % 3] = max(x, dp.get((index, x % 3), 0))
-
-        return dp[index, 0]
+        return dp[0]
