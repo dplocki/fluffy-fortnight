511fca871af123ae67db3ab3d6082b755532447c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-24 19:24:53 +0100

    Update binary-prefix-divisible-by-5.py

--- binary-prefix-divisible-by-5.py
+++ binary-prefix-divisible-by-5.py
@@ -1,6 +1,6 @@
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
         return list(map(
-            lambda n: n % 5 == 0,
-            accumulate(nums, lambda number, digit: number * 2 + digit)
+            lambda n: n == 0,
+            accumulate(nums, lambda number, digit: (number << 1 | digit) % 5)
         ))
