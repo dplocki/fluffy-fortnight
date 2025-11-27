a0727d77e400bef3e8037d84b376d666a24543e4
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-11-27 19:13:28 +0100

    Update maximum-subarray-sum-with-length-divisible-by-k.py

--- maximum-subarray-sum-with-length-divisible-by-k.py
+++ maximum-subarray-sum-with-length-divisible-by-k.py
@@ -3,14 +3,11 @@ class Solution:
         return max(self.internal(nums, k))
 
     def internal(self, nums: List[int], k: int) -> Iterable[int]:
-        n = len(nums)
-        nums_sums = list(accumulate(nums))
-        for start in range(0, n - k + 1):
-            for multiplayer in count(1):
-                s = nums_sums[start - 1] if start > 0 else 0
-                end = start + multiplayer * k - 1
-                if end >= n:
-                    break
+        min_prefix_sum = [float(inf)] * (k - 1) + [0]
+        accumulate_sum = 0
 
-                e = nums_sums[end]
-                yield e - s
+        for index, value in enumerate(nums):
+            accumulate_sum += value
+            remainder = index % k
+            yield accumulate_sum - min_prefix_sum[remainder]
+            min_prefix_sum[remainder] = min(min_prefix_sum[remainder], accumulate_sum)
