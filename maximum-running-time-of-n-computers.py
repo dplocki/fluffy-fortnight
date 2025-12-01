360d0ae94664f831f2b5ea4a6cde995313b51183
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-12-01 19:56:24 +0100

    Create maximum-running-time-of-n-computers.py

---
+++ maximum-running-time-of-n-computers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        left, right = 0, sum(batteries)
+
+        while left < right:
+            mid = (left + right + 1) // 2
+
+            gain_power = sum(min(bettery, mid) for bettery in batteries)
+
+            if gain_power >= n * mid:
+                left = mid
+            else:
+                right = mid - 1
+
+        return left
