deea92c465af09407f16978e818c5d459ee5b436
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-12-02 19:04:08 +0100

    Update count-number-of-trapezoids-i.py

--- count-number-of-trapezoids-i.py
+++ count-number-of-trapezoids-i.py
@@ -3,13 +3,10 @@ class Solution:
 
     def countTrapezoids(self, points: List[List[int]]) -> int:
         lines = defaultdict(int)
-
         for point in points:
             lines[point[1]] += 1
 
-        pairs_count = (
-            s * (s - 1) >> 1
-            for y, s in lines.items()
-        )
-
-        return sum((a * b) % Solution.MOD for a, b in combinations(pairs_count, 2))
+        pairs_count = [ s * (s - 1) >> 1 for s in lines.values()]
+        total_sum = sum(pairs_count)
+        sum_of_squares = sum(p * p for p in pairs_count)
+        return ((total_sum * total_sum - sum_of_squares) >> 1) % Solution.MOD
