610d43a67f17bec4ae31dcb90e8001f32982bdc9
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-12-08 21:10:08 +0100

    Update count-square-sum-triples.py

--- count-square-sum-triples.py
+++ count-square-sum-triples.py
@@ -1,9 +1,12 @@
 class Solution:
     def countTriples(self, n: int) -> int:
         result = 0
+        sqare_c = set(c * c for c in range(1, n + 1))
+
         for a in range(1, n + 1):
             for b in range(1, n + 1):
-                for c in range(1, n + 1):
-                    if a * a + b * b == c * c:
-                        result += 1
+                sum_a2_b2 = a * a + b * b
+                if sum_a2_b2 in sqare_c:
+                    result += 1
+
         return result
