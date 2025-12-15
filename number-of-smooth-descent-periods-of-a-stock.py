1dc2bf041448dffe297af80f69f09f954bb1b889
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-12-15 21:01:09 +0100

    Update number-of-smooth-descent-periods-of-a-stock.py

--- number-of-smooth-descent-periods-of-a-stock.py
+++ number-of-smooth-descent-periods-of-a-stock.py
@@ -2,12 +2,12 @@ class Solution:
     def getDescentPeriods(self, prices: List[int]) -> int:
         result = 0
         period = 1
-        for a, b in zip(prices,  prices[1:]):
+        for a, b in zip_longest(prices,  prices[1:], fillvalue=-1):
             if a - b == 1:
                 period += 1
-            else:
-                result += (period + 1) * period >> 1
-                period = 1
+                continue
 
-        result += (period + 1) * period >> 1
+            result += (period + 1) * period >> 1
+            period = 1
+
         return result
