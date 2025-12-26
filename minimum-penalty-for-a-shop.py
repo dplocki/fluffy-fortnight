37343d860f0f93ac11016489656cebf43bea36e2
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2025-12-26 20:24:40 +0100

    Update minimum-penalty-for-a-shop.py

--- minimum-penalty-for-a-shop.py
+++ minimum-penalty-for-a-shop.py
@@ -1,26 +1,7 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        customers_cames = [0]
-        customers_not_cames = [0]
-        clients_comming = 0
-        clients_not_comming = 0
-
-        for at_hour in customers:
-            if at_hour == 'Y':
-                clients_comming += 1
-            else:
-                clients_not_comming += 1
-
-            customers_cames.append(clients_comming)
-            customers_not_cames.append(clients_not_comming)
-
-        all_entries = customers_cames[-1] + customers_not_cames[-1]
-        result = 0
-        min_value = all_entries
-        for index, (c, nc) in enumerate(zip(customers_cames, customers_not_cames), 1):
-            tmp = (customers_cames[-1] - customers_cames[index - 1]) + customers_not_cames[index - 1]
-            if tmp < min_value:
-                result = index - 1
-                min_value = tmp
-
-        return result
+        return min(enumerate(
+                accumulate(customers, lambda p, c: p - 1 if c == 'Y' else p + 1, initial=0)
+            ),
+            key=lambda v: v[1]
+        )[0]
