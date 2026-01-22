9d939d86c7825d246f5eb943c14fcbc264fc7483
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-01-22 20:06:01 +0100

    Update minimum-pair-removal-to-sort-array-i.py

--- minimum-pair-removal-to-sort-array-i.py
+++ minimum-pair-removal-to-sort-array-i.py
@@ -9,18 +9,19 @@ class Solution:
 
             is_non_decreasing = True
             minimal_pair_sum = float('inf')
-            cache = {}
+            minimal_pair_sum_index = -1
             for index in range(n - 1, 0, -1):
                 if nums[index - 1] > nums[index]:
                     is_non_decreasing = False
+
                 pair_sum = nums[index - 1] + nums[index]
-                minimal_pair_sum = min(minimal_pair_sum, pair_sum)
-                cache[pair_sum] = index
+                if pair_sum <= minimal_pair_sum:
+                    minimal_pair_sum = pair_sum
+                    minimal_pair_sum_index = index
 
             if is_non_decreasing:
                 return result
 
             result += 1
-            index = cache[minimal_pair_sum]
-            nums = nums[:index - 1] + [minimal_pair_sum] + nums[index + 1:]
+            nums = nums[:minimal_pair_sum_index - 1] + [minimal_pair_sum] + nums[minimal_pair_sum_index + 1:]
             n -= 1
