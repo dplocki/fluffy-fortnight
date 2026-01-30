3a38620be2e61a8a59a915f9e956850e027951de
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-01-30 22:56:02 +0100

    Update minimum-cost-to-convert-string-ii.py

--- minimum-cost-to-convert-string-ii.py
+++ minimum-cost-to-convert-string-ii.py
@@ -11,15 +11,16 @@ class Solution:
             if node not in costs[node]:
                 costs[node][node] = 0
 
+        lengths = set()
         for a, b, c in zip(original, changed, cost):
             costs[a][b] = min(c, costs[a].get(b, Solution.INFINITY))
+            lengths.add(len(a))
 
         for b in nodes:
             for a in nodes:
                 for c in nodes:
                     costs[a][c] = min(costs[a].get(b, Solution.INFINITY) + costs[b].get(c, Solution.INFINITY), costs[a].get(c, Solution.INFINITY))
 
-        lengths = sorted(set(map(len, original)))
         n = len(source)        
         dp = { i:Solution.INFINITY for i in range(n + 1) }
         dp[0] = 0
@@ -34,12 +35,13 @@ class Solution:
             for length in lengths:
                 end = start + length
                 if end > n:
-                    break
-                
-                substring = source[start:end]
-                if substring not in costs:
                     continue
 
-                dp[end] = min(dp[end], dp[start] + costs[substring].get(target[start:end], Solution.INFINITY))
+                sub_source = source[start:end]
+                sub_target = target[start:end]
+                if sub_source not in costs or sub_target not in costs:
+                    continue
 
-        return  -1 if dp[n] == Solution.INFINITY else dp[n]
+                dp[end] = min(dp[end], dp[start] + costs[sub_source].get(sub_target, Solution.INFINITY))
+
+        return -1 if dp[n] == Solution.INFINITY else dp[n]
