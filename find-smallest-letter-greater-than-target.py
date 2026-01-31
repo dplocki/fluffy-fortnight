9720c2148de858d4ef169f79c0a3288b83138745
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-01-31 09:13:16 +0100

    Update find-smallest-letter-greater-than-target.py

--- find-smallest-letter-greater-than-target.py
+++ find-smallest-letter-greater-than-target.py
@@ -1,7 +1,18 @@
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for letter in letters:
-            if letter > target:
-                return letter
+        result = 0
+        left, right = 0, len(letters) - 1
+
+        while left <= right:
+            mid = (left + right) >> 1
+
+            if letters[mid] > target:
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        if result == -1:
+            return letters[0]
         
-        return letters[0]
+        return letters[result]
