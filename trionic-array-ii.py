2c00d348c283bdd6db661a0a053abc910ff51fc0
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-04 21:11:00 +0100

    Update trionic-array-ii.py

--- trionic-array-ii.py
+++ trionic-array-ii.py
@@ -2,43 +2,18 @@ class Solution:
     def maxSumTrionic(self, nums: List[int]) -> int:
         n = len(nums)
 
-        def build_phase_array(operator: Callable[[int, int], bool]) -> Dict[int, Tuple[int, int]]:
-            start, current_sum = None, 0
-            for index in range(1, n):
-                if operator(nums[index - 1], nums[index]):
-                    if start == None:
-                        start = index - 1
+        phase1 = phase2 = phase3 = result = -inf
+        for i in range(1, n):
+            first, second = nums[i - 1], nums[i]
 
-                    current_sum += nums[index]
-                elif start != None:
-                    yield start, index - 1, current_sum
-                    current_sum = 0
-                    start = None
+            current_phase1 = current_phase2 = current_phase3 = -inf
+            if first < second:
+                current_phase1 = max(first + second, phase1 + second)
+                current_phase3 = max(phase3 + second, phase2 + second)
+            elif first > second:
+                current_phase2 = max(phase2 + second, phase1 + second)
 
-            if start != None:
-                yield start, n - 1, current_sum
+            result = max(result, current_phase3)
+            phase1, phase2, phase3 = current_phase1, current_phase2, current_phase3
 
-        def internal(increasing: Dict[int, Tuple[int, int]], decreasing: Dict[int, Tuple[int, int]]) -> Generator[int, None, None]:
-            for array1_start, (array1_end, array1_value) in increasing.items():
-                if array1_end not in decreasing:
-                    continue
-
-                array2_end, array2_value = decreasing[array1_end]
-                if array2_end not in increasing:
-                    continue
-
-                current_sum = nums[array1_start] + array1_value + array2_value
-
-                for index in range(array2_end, increasing[array2_end][0]):
-                    current_sum += nums[index + 1]
-                    yield current_sum
-
-        increasing = {}
-        for start, end, value in build_phase_array(operator.lt):
-            for index in range(start, end):
-                increasing[index] = (end, value)
-                value -= nums[index + 1]
-
-        decreasing = { start: (end, value) for start, end, value in build_phase_array(operator.gt) }
-
-        return max(internal(increasing, decreasing))
+        return result
