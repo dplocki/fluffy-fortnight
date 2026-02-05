d99d94bc26aea5552af8f47f74f6a054ac031841
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-05 19:45:15 +0100

    Update transformed-array.py

--- transformed-array.py
+++ transformed-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        result = nums[:]
+        result = list()
         
         for index, value in enumerate(nums):
-            result[index] = nums[(index + value) % n]
+            result.append(nums[(index + value) % n])
 
         return result
