219e3bb2b7ed9e8b1d3dece48799fb15149355aa
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-07 08:21:33 +0100

    Update set-mismatch.py

--- set-mismatch.py
+++ set-mismatch.py
@@ -1,15 +1,14 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        numbers = set()
-        correct = set(range(1, n + 1))
+        numbers, correct = set(), set(range(1, n + 1))
         result = None
 
         for num in nums:
-            if num not in numbers:
+            if num in numbers:
+                result = num
+            else:
                 numbers.add(num)
                 correct.remove(num)
-            else:
-                result = num
 
         return [result, correct.pop()]
