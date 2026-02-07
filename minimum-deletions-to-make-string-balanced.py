9e7b48f1221279ebccb7aa94fc0594e6e3f9865a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-07 07:59:36 +0100

    Update minimum-deletions-to-make-string-balanced.py

--- minimum-deletions-to-make-string-balanced.py
+++ minimum-deletions-to-make-string-balanced.py
@@ -1,22 +1,11 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        b_before = []
-
-        n = 0
-        count = 0
+        result, count = 0, 0
         for letter in s:
             if letter == 'b':
                 count += 1
-
-            b_before.append(count)
-            n += 1
-       
-        result = n
-        count = 0
-        for index in range(n - 1, -1, -1):
-            if s[index] == 'a':
-                count += 1
-
-            result = min(result, b_before[index] + count - 1)
+            elif count > 0:
+                result += 1
+                count -= 1
 
         return result
