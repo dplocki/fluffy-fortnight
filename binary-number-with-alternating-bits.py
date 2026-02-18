d14ac1a433d6c0df095345d344065230b210d925
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-18 18:44:11 +0100

    Update binary-number-with-alternating-bits.py

--- binary-number-with-alternating-bits.py
+++ binary-number-with-alternating-bits.py
@@ -1,13 +1,4 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        prev = None
-
-        while n != 0:
-            bit = n % 2
-            if bit == prev:
-                return False
-
-            prev = bit
-            n >>= 1
-
-        return True
+        tmp = n ^ (n >> 1)
+        return tmp & (tmp + 1) == 0
