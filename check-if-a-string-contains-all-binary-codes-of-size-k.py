4c54665f47902b828b7a9e55cba8f4427df88e5b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-23 17:58:38 +0100

    Update check-if-a-string-contains-all-binary-codes-of-size-k.py

--- check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,11 +1,6 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        return len(set(Solution.sliding_window(s, k))) == 2 ** k
-
-    def sliding_window(iterable, n):
-        "Collect data into overlapping fixed-length chunks or blocks."
-        iterator = iter(iterable)
-        window = deque(islice(iterator, n - 1), maxlen=n)
-        for x in iterator:
-            window.append(x)
-            yield tuple(window)
+        return len(set(
+            s[i:i + k]
+            for i in range(len(s) - k + 1)
+        )) == 2 ** k
