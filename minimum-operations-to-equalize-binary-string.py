0dc7ba5897ff90b92788f70759bedd1165de7f6b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-27 19:55:07 +0100

    Update minimum-operations-to-equalize-binary-string.py

--- minimum-operations-to-equalize-binary-string.py
+++ minimum-operations-to-equalize-binary-string.py
@@ -1,26 +1,35 @@
 class Solution:
     def minOperations(self, s: str, k: int) -> int:
-        visited = set()
-        to_check = [(0, s.count('1'), s.count('0'))]
+        zeros = s.count('0')
+        length = len(s)
+        if zeros == 0:
+            return 0
 
-        while to_check:
-            steps, ones, zeros = heappop(to_check)
-            if (zeros, ones) in visited:
-                continue
+        if length == k:
+            return 1 if zeros == length else -1
 
-            visited.add((zeros, ones))
-            if zeros == 0:
-                return steps
+        base = length - k
+        odd = max(
+            (zeros + k - 1) // k,
+            (length - zeros + base - 1) // base
+        )
 
-            steps += 1
-            for d_zeros in range(min(zeros, k) + 1):
-                d_ones = k - d_zeros
-                if d_ones > ones:
-                    continue
+        if odd % 2 == 0:
+            odd += 1
 
-                new_zeros = zeros - d_zeros + d_ones
-                new_ones = ones - d_ones + d_zeros
-                if (new_zeros >= 0 and new_ones >= 0) and (new_zeros, new_ones) not in visited:
-                    heappush(to_check, (steps, new_ones, new_zeros))
+        even = max(
+            (zeros + k - 1) // k,
+            (zeros + base - 1) // base
+        )
 
-        return -1
+        if even % 2 == 1:
+            even += 1
+
+        result = float('inf')
+        if k % 2 == zeros % 2:
+            result = min(result, odd)
+
+        if zeros % 2 == 0:
+            result = min(result, even)
+
+        return -1 if result == float('inf') else result
