69b90c91ccbec4bcc36cb789710c92361225c3a0
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-28 07:23:29 +0100

    Update concatenation-of-consecutive-binary-numbers.py

--- concatenation-of-consecutive-binary-numbers.py
+++ concatenation-of-consecutive-binary-numbers.py
@@ -3,14 +3,7 @@ class Solution:
 
     def concatenatedBinary(self, n: int) -> int:
         result = 0
-        digits_number = 1
-        number_limit = 2
-
         for i in range(n + 1):
-            if i == number_limit:
-                digits_number += 1
-                number_limit <<= 1
-
-            result = ((result << digits_number) | i) % Solution.MODULO
+            result = ((result << i.bit_length()) | i) % Solution.MODULO
 
         return result
