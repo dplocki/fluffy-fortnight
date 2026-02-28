a6529db718ae21e772620c7c9269b435166ee283
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-02-28 07:18:12 +0100

    Create concatenation-of-consecutive-binary-numbers.py

---
+++ concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    MODULO = 10**9 + 7
+
+    def concatenatedBinary(self, n: int) -> int:
+        result = 0
+        digits_number = 1
+        number_limit = 2
+
+        for i in range(n + 1):
+            if i == number_limit:
+                digits_number += 1
+                number_limit <<= 1
+
+            result <<= digits_number
+            result += i
+
+        return result % Solution.MODULO
