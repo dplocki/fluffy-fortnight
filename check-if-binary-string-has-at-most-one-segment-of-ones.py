7313c0f004690e26b1ff366677f07e9700dc3395
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-06 16:59:10 +0100

    Update check-if-binary-string-has-at-most-one-segment-of-ones.py

--- check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,12 +1,3 @@
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        s = iter(s)
-
-        for d in takewhile(lambda d: d == '1', s):
-            pass
-
-        for d in s:
-            if d == '1':
-                return False
-        
-        return True
+        return '01' not in s
