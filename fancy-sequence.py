fa9195e62f86b3c6cd78f85e9b16580fecaa1d5c
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-15 11:08:43 +0100

    Update fancy-sequence.py

--- fancy-sequence.py
+++ fancy-sequence.py
@@ -3,22 +3,28 @@ class Fancy:
 
     def __init__(self):
         self.n = 0
+        self.operations_number = 0
         self.numbers = []
+        self.operations = []
 
     def append(self, val: int) -> None:
-        self.numbers.append(val)
+        self.numbers.append((val, self.operations_number))
         self.n += 1
 
     def addAll(self, inc: int) -> None:
-        for index, number in enumerate(self.numbers):
-            self.numbers[index] = (number + inc) % Fancy.MOD
+        self.operations.append(lambda x: (x + inc) % Fancy.MOD)
+        self.operations_number += 1
 
     def multAll(self, m: int) -> None:
-        for index, number in enumerate(self.numbers):
-            self.numbers[index] = (number * m) % Fancy.MOD
+        self.operations.append(lambda x: (x * m) % Fancy.MOD)
+        self.operations_number += 1
 
     def getIndex(self, idx: int) -> int:
         if idx >= self.n:
             return -1
 
-        return self.numbers[idx]
+        result, operation_start = self.numbers[idx]
+        for operation_index in range(operation_start, self.operations_number):
+            result = self.operations[operation_index](result)
+
+        return result
