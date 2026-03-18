c202058471d27d81f93518306c66a241149323cc
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-18 17:00:46 +0100

    Create count-submatrices-with-top-left-element-and-sum-less-than-k.py

---
+++ count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        previous_row = { -1: 0 }
+        result = 0
+
+        for r, row in enumerate(grid):
+            current_row = { -1: 0 }
+
+            for c, cell in enumerate(row):
+                current_sum = current_row[c - 1] + previous_row.get(c, 0) + cell - previous_row.get(c - 1, 0)
+                current_row[c] = current_sum
+
+                if current_sum > k:
+                    break
+
+                result += 1
+
+            previous_row = current_row
+
+        return result
