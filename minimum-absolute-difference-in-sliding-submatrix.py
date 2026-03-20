2d92a13da40567218f5bea03b83ca48a066bd92a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-20 18:08:01 +0100

    Update minimum-absolute-difference-in-sliding-submatrix.py

--- minimum-absolute-difference-in-sliding-submatrix.py
+++ minimum-absolute-difference-in-sliding-submatrix.py
@@ -1,9 +1,11 @@
 class Solution:
     def minAbsDiff(self, grid: List[List[int]], k: int) -> List[List[int]]:
         rows, columns = len(grid), len(grid[0])
-        result = [[0] * (columns - k + 1) for r in range(rows - k + 1)]
+        result = []
 
         for r in range(rows - k + 1):
+            row = []
+
             for c in range(columns - k + 1):
                 submatrix = set(
                     grid[r + rk][c + ck]
@@ -11,8 +13,10 @@ class Solution:
                     for rk in range(k)
                 )
 
-                result[r][c] = 0 if len(submatrix) == 1 else min(
+                row.append(0 if len(submatrix) == 1 else min(
                     map(lambda e: abs(e[0] - e[1]), combinations(submatrix, 2))
-                )
+                ))
+
+            result.append(row)
         
         return result
