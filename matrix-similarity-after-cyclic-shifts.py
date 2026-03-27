acf8a67c3106c8dff2fe2d579d86286c7e5dc5a2
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-27 17:44:22 +0100

    Update matrix-similarity-after-cyclic-shifts.py

--- matrix-similarity-after-cyclic-shifts.py
+++ matrix-similarity-after-cyclic-shifts.py
@@ -2,9 +2,7 @@ class Solution:
     def areSimilar(self, mat: List[List[int]], k: int) -> bool:
         m, n = len(mat), len(mat[0])
 
-        for row in mat:
-            for i in range(n):
-                if row[(i + k) % n] != row[i]:
-                    return False
-
-        return True
+        return all(
+            row[(i + k) % n] == row[i]
+            for row in mat
+            for i in range(n))
