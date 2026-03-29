b8c6e795489292ea40ff201df5d51478a6e35ed1
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-03-29 09:13:33 +0200

    Update check-if-strings-can-be-made-equal-with-operations-i.py

--- check-if-strings-can-be-made-equal-with-operations-i.py
+++ check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,3 +1,3 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        return Counter(s1[::2]) == Counter(s2[::2]) and Counter(s1[1::2]) == Counter(s2[1::2])
+        return sorted([s1[0], s1[2]]) == sorted([s2[0], s2[2]]) and sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]])
