ccb1ea13e6369303d12b8bd1d56fd6fb021912b8
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-04-07 23:44:35 +0200

    Update walking-robot-simulation-ii.py

--- walking-robot-simulation-ii.py
+++ walking-robot-simulation-ii.py
@@ -1,32 +1,39 @@
-DIRECTIONS = { 0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0) }
 DIRECTIONS_LABELS = ("North", "East", "South", "West")
 
 class Robot:
 
     def __init__(self, width: int, height: int):
-        self.directory = 1
-        self.x = 0
-        self.y = 0
-        self.width = width
-        self.height = height
+        self.x, self.y, self.directory = 0, 0, 1
+        self.index = 0
+        self.whole_border = 2 * width + 2 * height - 4
+        self.border = { 0: (0, 0, 2) }
+
+        index = 0
+        for i in range(1, width):
+            index += 1
+            self.border[index] = (i, 0, 1)
+
+        for i in range(1, height):
+            index += 1
+            self.border[index] = (width - 1, i, 0)
+
+        for i in range(width - 2, -1, -1):
+            index += 1
+            self.border[index] = (i, height - 1, 3)
+
+        for i in range(height - 2, 0, -1):
+            index += 1
+            self.border[index] = (0, i, 2)
 
     def step(self, num: int) -> None:
-        dx, dy = DIRECTIONS[self.directory]
+        if num == 0:
+            return
 
-        for _ in range(num):
-            tx = self.x + dx
-            ty = self.y + dy
-            if not ((0 <= tx < self.width) and (0 <= ty < self.height)):
-                self.directory = (self.directory - 1) % 4
-                dx, dy = DIRECTIONS[self.directory]
-                self.x += dx
-                self.y += dy
-            else:
-                self.x = tx
-                self.y = ty
+        self.index = (self.index + num) % self.whole_border
+        self.x, self.y, self.directory = self.border[self.index]
 
     def getPos(self) -> List[int]:
-        return (self.x, self.y)
+        return self.x, self.y
 
     def getDir(self) -> str:
         return DIRECTIONS_LABELS[self.directory]
