c554f84a802d364a164a826d09249b86744314e3
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-04-08 17:26:19 +0200

    Create xor-after-range-multiplication-queries-i.py

---
+++ xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    MOD = 10**9 + 7
+
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        for l, r, k, v in queries:
+            for i in range(l, r + 1, k):
+                nums[i] = (nums[i] * v) % Solution.MOD
+
+        return reduce(operator.xor, nums)
