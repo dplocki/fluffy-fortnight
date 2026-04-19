413a869ea8b2a6a4f7b9dc3879d911e4275181a8
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-04-19 09:46:02 +0200

    Update maximum-distance-between-a-pair-of-values.py

--- maximum-distance-between-a-pair-of-values.py
+++ maximum-distance-between-a-pair-of-values.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
-        result, n1, n2 = 0, len(nums1), len(nums2)
-        for i in range(min(n1, n2)):
-            for j in range(i, n2):
-                if nums1[i] <= nums2[j]:
-                    result = max(result, j - i)
-        
-        return result
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] > nums2[j]:
+                i += 1
+
+            j += 1
+
+        return max(0, j - i - 1)
