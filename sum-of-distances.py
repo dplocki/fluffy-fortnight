d117619af8f36919efad0844f3951cefa27d5ddb
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-04-23 19:59:51 +0200

    Update sum-of-distances.py

--- sum-of-distances.py
+++ sum-of-distances.py
@@ -1,14 +1,23 @@
 class Solution:
     def distance(self, nums: List[int]) -> List[int]:
-        indexes = {}
+        indexes = defaultdict(list)
+        for index, value in enumerate(nums):
+            indexes[value].append(index)
+      
+        result = [0] * len(nums)
+        for indices in indexes.values():
+            index_count = len(indices)
+            left_sum = 0
+            right_sum = sum(indices) - index_count * indices[0]
+          
+            for i in range(index_count):
+                result[indices[i]] = left_sum + right_sum
+              
+                if i + 1 >= index_count:
+                    continue
 
-        for index, num in enumerate(nums):
-            if num not in indexes:
-                indexes[num] = []
-
-            indexes[num].append(index)
-
-        return [
-            sum(abs(index - i) for i in indexes[num])
-            for index, num in enumerate(nums)
-        ]
+                gap = indices[i + 1] - indices[i]
+                left_sum += gap * (i + 1)
+                right_sum -= gap * (index_count - i - 1)
+    
+        return result
