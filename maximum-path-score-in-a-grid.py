5535f9a6ec1ebc035d6089ae4ecfe3e8995c96b5
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-04-30 21:30:20 +0200

    Update maximum-path-score-in-a-grid.py

--- maximum-path-score-in-a-grid.py
+++ maximum-path-score-in-a-grid.py
@@ -1,29 +1,29 @@
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
+        rows, columns = len(grid), len(grid[0])
         dp = {}
-        for i in range(k + 1):
-            dp[-1, 0, i] = 0
-            dp[0, -1, i] = 0
 
-        m = 0
-        for row_index, row in enumerate(grid):
-            m += 1
+        @cache
+        def internal(row: int, column: int, cost: int) -> int:
+            if row >= rows or column >= columns:
+                return -1
 
-            n = 0
-            for column_index, value in enumerate(row):
-                n += 1
+            new_cost = cost + (grid[row][column] > 0)
+            if new_cost > k:
+                return -1
 
-                if value == 0:
-                    for i in range(k + 1):
-                        dp[row_index, column_index, i] = max(dp.get((row_index - 1, column_index, i), -1), dp.get((row_index, column_index - 1, i), -1))
-                else:
-                    for i in range(1, k + 1):
-                        top = dp.get((row_index - 1, column_index, i - 1), -1)
-                        top = -1 if top == -1 else top + value
+            if row == rows - 1 and column == columns - 1:
+                return grid[row][column]
 
-                        left = dp.get((row_index, column_index - 1, i - 1), -1)
-                        left = -1 if left == -1 else left + value
+            down = internal(row + 1, column, new_cost)
+            right = internal(row, column + 1, new_cost)
 
-                        dp[row_index, column_index, i] = max(top, left)
+            if down == -1 and right == -1:
+                return -1
 
-        return max(dp.get((m - 1, n - 1, i), -1) for i in range(k + 1))
+            return max(down, right) + grid[row][column]
+
+        try:
+            return internal(0, 0, 0)
+        finally:
+            internal.cache_clear()
