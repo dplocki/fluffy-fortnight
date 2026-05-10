eca61dc0e44f58278c29ab196e40dd96a8cab9d4
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-10 07:34:55 +0200

    Create maximum-number-of-jumps-to-reach-the-last-index.py

---
+++ maximum-number-of-jumps-to-reach-the-last-index.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = { 0: 0 }
+
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if abs(nums[j] - nums[i]) > target:
+                    continue
+
+                if i not in dp:
+                    continue
+
+                dp[j] = max(dp.get(j, -1), dp[i] + 1)
+
+        return dp.get(n - 1, -1)
