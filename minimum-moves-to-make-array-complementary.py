59e3b0189670575afdb272c59901b96ae79860d6
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-13 18:16:05 +0200

    Create minimum-moves-to-make-array-complementary.py

---
+++ minimum-moves-to-make-array-complementary.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        delta = defaultdict(int)
+
+        for i in range(n // 2):
+            mini = min(nums[i], nums[n -1 - i])
+            maxi = max(nums[i], nums[n -1 - i])
+
+            delta[2] +=  2
+            delta[mini + 1] -= 1
+            delta[mini + maxi] -= 1
+            delta[mini + maxi + 1] += 1
+            delta[maxi + limit + 1] += 1
+
+        result = n
+        moves = 0
+
+        for targ in range(2, 2 * limit + 1):
+            moves += delta[targ]
+            result = min(result, moves)
+
+        return result
