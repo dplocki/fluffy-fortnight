dbf6aed8cf68053da5913dc841ab41fea1e6ced0
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-19 18:11:53 +0200

    Update minimum-common-value.py

--- minimum-common-value.py
+++ minimum-common-value.py
@@ -3,14 +3,16 @@ class Solution:
         iter1 = iter(nums1)
         iter2 = iter(nums2)
         
-        num1 = next(iter1, -1)
-        num2 = next(iter2, -1)
+        num1 = next(iter1, None)
+        num2 = next(iter2, None)
 
-        while num1 != num2:
+        while num1 != None and num2 != None:
+            if num1 == num2:
+                return num1
+
             if num1 < num2:
-                num1 = next(iter1, -1)
+                num1 = next(iter1, None)
+            elif num1 > num2:
+                num2 = next(iter2, None)
 
-            if num2 < num1:
-                num2 = next(iter2, -1)
-
-        return num1
+        return -1
