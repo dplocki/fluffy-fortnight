0f0c0e29cb8d53f6e99c57482ddaf6584d76b24e
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-21 20:13:13 +0200

    Update find-the-length-of-the-longest-common-prefix.py

--- find-the-length-of-the-longest-common-prefix.py
+++ find-the-length-of-the-longest-common-prefix.py
@@ -2,12 +2,24 @@ class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
         prefixes_1, prefixes_2 = set(), set()
 
-        for num in map(str, arr1):
-            for i in range(len(num)):
-                prefixes_1.add(num[:i + 1])
+        for num in arr1:
+            while num > 0:
+                if num in prefixes_1:
+                    break
 
-        for num in map(str, arr2):
-            for i in range(len(num)):
-                prefixes_2.add(num[:i + 1])
+                prefixes_1.add(num)
+                num //= 10
 
-        return max(map(len, prefixes_1 & prefixes_2), default=0)
+        for num in arr2:
+            while num > 0:
+                if num in prefixes_2:
+                    break
+                    
+                prefixes_2.add(num)
+                num //= 10
+
+        prefixes = prefixes_1 & prefixes_2
+        if not prefixes:
+            return 0
+
+        return len(str(max(prefixes)))
