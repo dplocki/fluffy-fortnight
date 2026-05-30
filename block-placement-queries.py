a809c47fa0baf8a716bf34f2ed0c090805aa971a
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-30 12:32:10 +0200

    Update block-placement-queries.py

--- block-placement-queries.py
+++ block-placement-queries.py
@@ -1,18 +1,33 @@
-from dataclasses import dataclass, field
-
-@dataclass
 class Node:
     left_border: int
     right_border: int
-    middle: int = field(default=None)
-    left: Node = field(default=None)
-    right: Node = field(default=None)
+    max_fit: int
+    middle: int = None
+    left: Node = None
+    right: Node = None
+    parent: Node = None
 
+    def __init__(self, left: int, right: int, parent: None):
+        self.left_border = left
+        self.right_border = right
+        self.max_fit = right - left
+        self.parent = parent
+    
     def split(self, x: int) -> None:
-        if self.middle == None:
+        if self.middle is None:
             self.middle = x
-            self.left = Node(self.left_border, x)
-            self.right = Node(x, self.right_border)
+            self.left = Node(self.left_border, x, self)
+            self.right = Node(x, self.right_border, self)
+            self.max_fit = max(self.left.max_fit, self.right.max_fit)
+
+            tmp = self.parent
+            while tmp:
+                new_fit = max(tmp.left.max_fit, tmp.right.max_fit)
+                if new_fit == tmp.max_fit:
+                    break
+                tmp.max_fit = new_fit
+                tmp = tmp.parent
+
             return
 
         if x < self.middle:
@@ -26,12 +41,15 @@ class Node:
         if self.middle == None:
             return min(self.right_border, x) - self.left_border >= size
 
-        return self.left.check(x, size) or self.right.check(x, size) 
+        if x < self.middle:
+            return self.left.check(x, size)
+
+        return self.left.max_fit >= size or self.right.check(x, size)
 
 
 class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
-        tree = Node(0, inf)
+        tree = Node(0, inf, None)
         result = []
 
         for query in queries:
