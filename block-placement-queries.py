6bea50ff95182a4e5cc80ffec9a4b8471f41d87e
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-05-30 13:12:14 +0200

    Update block-placement-queries.py

--- block-placement-queries.py
+++ block-placement-queries.py
@@ -1,61 +1,50 @@
+from sortedcontainers import SortedList
+
+
 class Node:
-    left_border: int
-    right_border: int
-    max_fit: int
-    middle: int = None
-    left: Node = None
-    right: Node = None
-    parent: Node = None
+    def __init__(self, n: int):
+        self.n = 1 << n.bit_length()
+        self.tree = [0] * (self.n << 1)
 
-    def __init__(self, left: int, right: int, parent: None):
-        self.left_border = left
-        self.right_border = right
-        self.max_fit = right - left
-        self.parent = parent
+    def update(self, x: int, value: int):
+        x += self.n
+        self.tree[x] = value
+        while x > 1:
+            x //= 2
+            self.tree[x] = max(self.tree[x << 1], self.tree[(x << 1) + 1])
     
-    def split(self, x: int) -> None:
-        if self.middle is None:
-            self.middle = x
-            self.left = Node(self.left_border, x, self)
-            self.right = Node(x, self.right_border, self)
-            self.max_fit = max(self.left.max_fit, self.right.max_fit)
+    def check(self, x: int) -> int:
+        x += self.n
+        res = self.tree[x]
+        while x > 1:
+            if x & 1:
+                res = max(res, self.tree[x - 1])
 
-            tmp = self.parent
-            while tmp:
-                new_fit = max(tmp.left.max_fit, tmp.right.max_fit)
-                if new_fit == tmp.max_fit:
-                    break
-                tmp.max_fit = new_fit
-                tmp = tmp.parent
-
-            return
-
-        if x < self.middle:
-            self.left.split(x)
-            return
-
-        if x > self.middle:
-            self.right.split(x)
-
-    def check(self, x: int, size: int) -> bool:
-        if self.middle == None:
-            return min(self.right_border, x) - self.left_border >= size
-
-        if x < self.middle:
-            return self.left.check(x, size)
-
-        return self.left.max_fit >= size or self.right.check(x, size)
-
+            x //= 2
+        return res
 
 class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
-        tree = Node(0, inf, None)
-        result = []
+        axis = SortedList()
+        tree = Node(max(q[1] for q in queries))
 
+        axis.add(0)
+        tree.update(0,0)
+
+        result = []
         for query in queries:
             if query[0] == 1:
-                tree.split(query[1])
-            elif query[0] == 2:
-                result.append(tree.check(query[1], query[2]))
+                ind = axis.bisect(query[1])
+                if ind < len(axis):
+                    nxt = axis[ind]
+                    tree.update(nxt, nxt - query[1])
+
+                tree.update(query[1], query[1] - axis[ind-1])
+                axis.add(query[1])
+            else:
+                previous = axis[axis.bisect(query[1]) - 1]
+                maxium = max(query[1] - previous, tree.check(previous))
+
+                result.append(query[2] <= maxium)
 
         return result
