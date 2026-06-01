1119ae1f6e3c394656975488b3af67c33487336f
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-06-01 17:25:29 +0200

    Update minimum-cost-of-buying-candies-with-discount.py

--- minimum-cost-of-buying-candies-with-discount.py
+++ minimum-cost-of-buying-candies-with-discount.py
@@ -1,9 +1,6 @@
 class Solution:
     def minimumCost(self, cost: List[int]) -> int:
-        result = 0
-
-        for index, candy in enumerate(sorted(cost, reverse=True)):
-            if index % 3 != 2:
-                result += candy
-        
-        return result
+        return sum(candy
+                for index, candy in enumerate(sorted(cost, reverse=True))
+                if index % 3 != 2
+            )
