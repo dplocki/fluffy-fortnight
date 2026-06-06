39405a1c0fd398b4191a038506c204613a25b2a2
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-06-06 07:19:56 +0200

    Update left-and-right-sum-differences.py

--- left-and-right-sum-differences.py
+++ left-and-right-sum-differences.py
@@ -1,15 +1,6 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        left_sum = []
-        current = 0
-        for num in nums:
-            left_sum.append(current)
-            current += num
-
-        right_sum = []
-        current = 0
-        for num in reversed(nums):
-            right_sum.append(current)
-            current += num
+        left_sum = left_sum = [0] + list(accumulate(nums[:-1]))
+        right_sum = [0] + list(accumulate(reversed(nums)))[:-1]
         
         return [abs(l - r) for l, r in zip(left_sum, reversed(right_sum))]
