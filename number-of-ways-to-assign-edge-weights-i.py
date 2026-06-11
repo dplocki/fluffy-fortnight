a242a639c13320fe6609923aecc57ad07274d645
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-06-11 17:56:03 +0200

    Create number-of-ways-to-assign-edge-weights-i.py

---
+++ number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,29 @@
+MOD = 1_000_000_007
+
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        nodes = defaultdict(set)
+        for node_a, node_b in edges:
+            nodes[node_a].add(node_b)
+            nodes[node_b].add(node_a)
+
+        nodes_depth = {}
+        to_check = [(1, 0)]
+        max_depth = 0
+
+        while to_check:
+            current_node, current_deep = to_check.pop()
+            if current_node in nodes_depth:
+                continue
+            
+            nodes_depth[current_node] = current_deep
+            max_depth = max(max_depth, current_deep)
+
+            new_deep = current_deep + 1
+            for new_node in nodes[current_node]:
+                if new_node in nodes_depth:
+                    continue
+
+                to_check.append((new_node, new_deep))
+
+        return (2 ** (max_depth - 1)) % MOD
