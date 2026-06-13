61e71338571aace8f457314b4a55f23f0878b8c5
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-06-13 07:40:31 +0200

    Update weighted-word-mapping.py

--- weighted-word-mapping.py
+++ weighted-word-mapping.py
@@ -1,9 +1,10 @@
+LETTERS_START = ord('a')
+
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
         return ''.join(
-            chr(ord('a') + 25 - (self.word_weight(weights, word) % 26))
+            chr(
+                LETTERS_START + 25 - (sum(weights[ord(letter) - LETTERS_START] for letter in word) % 26)
+            )
             for word in words
         )
-
-    def word_weight(self, weights: List[int], word: str) -> int:
-        return sum(weights[ord(letter) - ord('a')] for letter in word)
