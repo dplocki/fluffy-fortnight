d5f518f7c5d2640abdf059e4e48921121b6bf1b5
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-06-20 08:29:17 +0200

    Update maximum-building-height.py

--- maximum-building-height.py
+++ maximum-building-height.py
@@ -1,32 +1,18 @@
 class Solution:
     def maxBuilding(self, n: int, restrictions: List[List[int]]) -> int:
-        restrictions = { id: height for id, height in restrictions }
-        restrictions[1] = 0
-        to_check, visited = [], set()
-        heappush(to_check, (0, 1, 0))
+        restrictions.append([1, 0])
+        restrictions.sort()
+        if restrictions[-1][0] != n:
+            restrictions.append([n, n - 1])
 
-        while to_check:
-            negative_max_height, current_id, current_height = heappop(to_check)
-            if (current_id, current_height) in visited:
-                continue
+        restrictions_len = len(restrictions)
+        for index in range(1, restrictions_len):
+            restrictions[index][1] = min(restrictions[index][1], restrictions[index - 1][1] + restrictions[index][0] - restrictions[index - 1][0])
 
-            visited.add((current_id, current_height))
+        for index in range(restrictions_len - 2, 0, -1):
+            restrictions[index][1] = min(restrictions[index][1], restrictions[index + 1][1] + (restrictions[index + 1][0] - restrictions[index][0]))
 
-            next_id = current_id + 1
-            if next_id > n:
-                return -negative_max_height
-
-            next_height = current_height + 1
-            next_max_height = max(-negative_max_height, next_height)
-            if next_height <= restrictions.get(next_id, inf) and (next_id, next_height) not in visited:
-                heappush(to_check, (-next_max_height, next_id, next_height))
-
-            next_height = current_height
-            if 0 <= next_height <= restrictions.get(next_id, inf) and (next_id, next_height) not in visited:
-                heappush(to_check, (negative_max_height, next_id, next_height))
-
-            next_height = current_height - 1
-            if 0 <= next_height <= restrictions.get(next_id, inf) and (next_id, next_height) not in visited:
-                heappush(to_check, (negative_max_height, next_id, next_height))
-
-        return 0
+        return max(
+            ((restrictions[index + 1][0] - restrictions[index][0]) + restrictions[index][1] + restrictions[index + 1][1]) // 2
+            for index in range(restrictions_len - 1)
+        )
