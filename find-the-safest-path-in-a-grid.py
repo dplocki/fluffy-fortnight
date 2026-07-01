405a3ddefd393ddaf6aa007920d576816acee7e7
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-07-01 17:41:16 +0200

    Update find-the-safest-path-in-a-grid.py

--- find-the-safest-path-in-a-grid.py
+++ find-the-safest-path-in-a-grid.py
@@ -3,32 +3,42 @@ DIR = ((0, -1), (0, 1), (1, 0), (-1, 0))
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        thieves = [
-            (row_index, column_index)
-            for row_index, row in enumerate(grid)
-            for column_index, cell in enumerate(row)
-            if cell == 1
-        ]
+        safeness = {}
+        to_checked = deque()
 
-        @cache
-        def get_safeness_factor(row_index: int, column_index: int) -> int:
-            return min(
-                abs(row_index - thief_row) + abs(column_index - thief_column)
-                for thief_row, thief_column in thieves
-            )
+        for row in range(n):
+            for column in range(n):
+                if grid[row][column] == 1:
+                    safeness[row, column] = 0
+                    to_checked.append((row, column))
 
-        to_check = []
-        result = { (0, 0): get_safeness_factor(0, 0) }
-        heappush(to_check, (result[0, 0], 0, 0))
+        while to_checked:
+            row, column = to_checked.popleft()
+            for drow, dcolumn in DIR:
+                new_row, new_column = row + drow, column + dcolumn
+                if 0 <= new_row < n and 0 <= new_column < n and (new_row, new_column) not in safeness:
+                    safeness[new_row, new_column] = safeness[row, column] + 1
+                    to_checked.append((new_row, new_column))
 
-        while to_check:
-            current_path_safeness_factor, row_index, column_index = heappop(to_check)
+        distance = {}
+        distance[0, 0] = safeness[0, 0]
+        max_heap = [(-distance[0, 0], 0, 0)]
+
+        while max_heap:
+            neg_safe_factor, row, column = heapq.heappop(max_heap)
+            safe_factor = -neg_safe_factor
+            if safe_factor < distance.get((row, column), -1):
+                continue
+
+            if (row, column) == (n - 1, n - 1):
+                return safe_factor
 
             for drow, dcolumn in DIR:
-                new_row_index, new_column_index = row_index + drow, column_index + dcolumn
-                if (0 <= new_row_index < n) and (0 <= new_column_index < n) and ((new_row_index, new_column_index) not in result or result[new_row_index, new_column_index] < current_path_safeness_factor):
-                    new_path_safeness_factor = min(current_path_safeness_factor, get_safeness_factor(new_row_index, new_column_index))
-                    result[new_row_index, new_column_index] = new_path_safeness_factor
-                    heappush(to_check, (new_path_safeness_factor, new_row_index, new_column_index))
+                new_row, new_column = row + drow, column + dcolumn
+                if 0 <= new_row < n and 0 <= new_column < n:
+                    new_safe_factor = min(safe_factor, safeness[new_row, new_column])
+                    if new_safe_factor > distance.get((new_row, new_column), -1):
+                        distance[new_row, new_column] = new_safe_factor
+                        heapq.heappush(max_heap, (-new_safe_factor, new_row, new_column))
 
-        return result[n - 1, n - 1]
+        return distance[n - 1][n - 1]
