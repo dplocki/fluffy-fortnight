c90a54bf88373ba659d462b5fc45c0eb6ff5e691
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-07-07 17:57:46 +0200

    Update concatenate-non-zero-digits-and-multiply-by-sum-i.py

--- concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,8 +1,7 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
+        if n == 0:
+            return 0
+
         digits = list(map(int, str(n)))
-        x_str = ''.join(map(str, filter(lambda d: d != 0, digits)))
-        if x_str:
-            return int(x_str) * sum(digits)
-        
-        return 0
+        return int(''.join(map(str, filter(lambda d: d != 0, digits)))) * sum(digits)
