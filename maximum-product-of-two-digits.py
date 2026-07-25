4431650b64879e4e568581ffea5fd8cf75df412b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-07-25 07:09:43 +0200

    Update maximum-product-of-two-digits.py

--- maximum-product-of-two-digits.py
+++ maximum-product-of-two-digits.py
@@ -1,4 +1,3 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        sorted_digits = sorted(map(int, str(n)))[-2:]
-        return sorted_digits[-1] * sorted_digits[-2]
+        return operator.mul(*sorted(map(int, str(n)))[-2:])
