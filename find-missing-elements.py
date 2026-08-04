cbc867c520b2c88294a098034af1c75f61709b7b
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-08-04 17:48:38 +0200

    Update find-missing-elements.py

--- find-missing-elements.py
+++ find-missing-elements.py
@@ -1,3 +1,13 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        return list(sorted(set(range(min(nums), max(nums) + 1)) - set(nums)))
+        nums.sort()
+        expected = nums[0]
+        result = []
+
+        for num in nums:
+            while expected != num:
+                result.append(expected)
+                expected += 1
+            expected += 1
+
+        return result
