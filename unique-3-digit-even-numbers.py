2af0b82c2ad47cd518aceee249319508723aeb77
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-09-11 17:45:01 +0200

    Update unique-3-digit-even-numbers.py

--- unique-3-digit-even-numbers.py
+++ unique-3-digit-even-numbers.py
@@ -1,8 +1,7 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
         numbers = set(
-            int(''.join(p))
-            for p in permutations(map(str, digits), 3)
+            map(lambda p: int(''.join(p)), permutations(map(str, digits), 3))
         )
 
         return sum(1 for n in numbers if n & 1 == 0 and n > 99)
