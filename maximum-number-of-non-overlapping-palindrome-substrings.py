28133d9ce87027cba02b9d955555dd1bfcb1d7e7
Dawid Płocki <16208274+dplocki@users.noreply.github.com>
2026-09-15 19:00:50 +0200

    Update maximum-number-of-non-overlapping-palindrome-substrings.py

--- maximum-number-of-non-overlapping-palindrome-substrings.py
+++ maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -1,19 +1,19 @@
 class Solution:
     def maxPalindromes(self, s: str, k: int) -> int:
         n = len(s)
-        is_palindrome = defaultdict(bool)
+        is_palindrome = [[False] * n for _ in range(n)]
 
         for length in range(1, n + 1):
             for left in range(n - length + 1):
                 right = left + length - 1
-                is_palindrome[left, right] = s[left] == s[right] and (length <= 2 or is_palindrome[left + 1, right - 1])
+                is_palindrome[left][right] = s[left] == s[right] and (length <= 2 or is_palindrome[left + 1][right - 1])
 
         dp = defaultdict(int)
         for right in range(n):
             dp[right + 1] = dp[right]
 
             for left in range(right - k + 2):
-                if not is_palindrome[left, right]:
+                if not is_palindrome[left][right]:
                     continue
                 
                 dp[right + 1] = max(dp[right + 1], dp[left] + 1)
